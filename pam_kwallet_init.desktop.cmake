c974341fe3502c46d275ff56ae36477ed7cc573d
Armin K <krejzi@email.com>
2016-01-26 12:16:43 +0100

    Make kwallet-pam usable outside of Plasma
    REVIEW: 126843

---
+++ pam_kwallet_init.desktop.cmake
@@ -0,0 +1,8 @@
+[Desktop Entry]
+Name=KWallet PAM Socket Connection
+Comment=Connect to KWallet PAM socket
+Exec=${CMAKE_INSTALL_FULL_LIBEXECDIR}/pam_kwallet_init
+Type=Application
+NoDisplay=true
+X-KDE-StartupNotify=false
+X-KDE-autostart-phase=0
