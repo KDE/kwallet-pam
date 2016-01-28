8ed21bde5371b05637f4cf1c64dd6e80180a268f
l10n daemon script <scripty@kde.org>
2016-01-28 09:52:45 +0000

    SVN_SILENT made messages (.desktop file) - always resolve ours
    In case of conflict in i18n, keep the version of the branch "ours"
    To resolve a particular conflict, "git checkout --ours path/to/file.desktop"

--- pam_kwallet_init.desktop.cmake
+++ pam_kwallet_init.desktop.cmake
@@ -1,6 +1,18 @@
 [Desktop Entry]
 Name=KWallet PAM Socket Connection
+Name[ca]=Connexió a sòcol PAM del KWallet
+Name[nl]=KWallet verbinding met PAM-socket
+Name[pt]=Ligação por 'Socket' entre PAM e o KWallet
+Name[pt_BR]=Conexão do Socket PAM do KWallet
+Name[uk]=З’єднання із сокетом PAM KWallet
+Name[x-test]=xxKWallet PAM Socket Connectionxx
 Comment=Connect to KWallet PAM socket
+Comment[ca]=Connecta a un sòcol PAM del KWallet
+Comment[nl]=Verbinden met KWallet PAM-socket
+Comment[pt]=Ligar ao 'socket' de PAM do KWallet
+Comment[pt_BR]=Conecta ao socket PAM do KWallet
+Comment[uk]=З’єднатися із сокетом PAM KWallet
+Comment[x-test]=xxConnect to KWallet PAM socketxx
 Exec=${CMAKE_INSTALL_FULL_LIBEXECDIR}/pam_kwallet_init
 Type=Application
 NoDisplay=true
